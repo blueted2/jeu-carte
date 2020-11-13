c979f681caaf31f4d61c046b04203f5e200fbcc7
Gregory Holder <gregory.holder76@gmail.com>
2020-11-13 22:15:49 +0100

    Representation des cartes avec symboles

--- Jeu/src/fr/utt/sh/console_ui/VisitorAffichageString.java
+++ Jeu/src/fr/utt/sh/console_ui/VisitorAffichageString.java
@@ -2,42 +2,73 @@ package fr.utt.sh.console_ui;
 
 import fr.utt.sh.core.Carte;
 import fr.utt.sh.core.Tapis_5x3;
+import fr.utt.sh.core.Carte.Forme;
 import fr.utt.sh.core.Carte.Remplissage;
 
-public class VisitorAffichageString implements VisitorAffichage{
-	
+public class VisitorAffichageString implements VisitorAffichage {
+
 	private String representationString;
 
 	public void visit(Tapis_5x3 tapis) {
 		representationString = "";
-		
 		representationString += "----------------------\n";
-		
 		for (int y = 0; y < 5; y++) {
 			representationString += "|";
 			for (int x = 0; x < 7; x++) {
 				Carte carte = tapis.getCarteAt(x, y);
-				
+
 				String strCarte;
-				if(carte == null) {
+				if (carte == null) {
 					strCarte = "  ";
-				}else {
+				} else {
 					strCarte = getRepresentationString(carte);
 				}
-				
+
 				representationString += strCarte + "|";
 			}
-			
 			representationString += "\n" + "----------------------\n";
 		}
 	}
 
 	public void visit(Carte carte) {
 		char charCouleur = carte.getCouleur().name().charAt(0);
-		char charRemlissage = (carte.getRemplissage() == Carte.Remplissage.Vide) ? ' ' : 'X';
-		representationString = String.format("%s%s", charCouleur, charRemlissage);
+//		char charRemlissage = (carte.getRemplissage() == Carte.Remplissage.Vide) ? ' ' : 'X';
+
+		String charForme = "?";
+		if (carte.getRemplissage() == Remplissage.Rempli) {
+			switch (carte.getForme()) {
+				case Carre:
+					charForme = "■";
+					break;
+				case Cercle:
+					charForme = "▲";
+					break;
+				case Triangle:
+					charForme = "●";
+					break;
+				default:
+					break;
+
+			}
+		} else {
+			switch (carte.getForme()) {
+				case Carre:
+					charForme = "□";
+					break;
+				case Cercle:
+					charForme = "△";
+					break;
+				case Triangle:
+					charForme = "○";
+					break;
+				default:
+					break;
+			}
+		}
+
+		representationString = String.format("%s%s", charCouleur, charForme);
 	}
-	
+
 	public static String getRepresentationString(VisitableAffichage visitable) {
 		VisitorAffichageString vis = new VisitorAffichageString();
 		visitable.accept(vis);
