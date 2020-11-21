a8d2de4043dc6fbd008202af0c2394360f40bf50
Gregory Holder <gregory.holder76@gmail.com>
2020-11-22 00:18:09 +0100

    Eneleve vieu code

--- Jeu/src/fr/utt/sh/core/Main.java
+++ Jeu/src/fr/utt/sh/core/Main.java
@@ -10,21 +10,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		ControlleurJeu c = ControlleurJeu.getInstance();
-		c.commencerNouvellePartie(1, 1);		
+		c.commencerNouvellePartie(2, 0);		
 		while (!c.tapisEstRempli()) {
 			c.jouer();
 		}
-		
-//		Tapis tapis = new Tapis_Triangulaire(5);
-//		
-//		System.out.println(VisitorAffichageString.getRepresentationStringStatic(tapis));
-//		
-//		Carte c = new Carte(Carte.Couleur.Rouge, Carte.Remplissage.Rempli, Carte.Forme.Carre);
-//		
-//		tapis.poserCarte(c, 0, 3);
-//		System.out.println(VisitorAffichageString.getRepresentationStringStatic(tapis));
-//		
-//		tapis.poserCarte(c, 1, 0);
-//		System.out.println(VisitorAffichageString.getRepresentationStringStatic(tapis));
 	}
 }
