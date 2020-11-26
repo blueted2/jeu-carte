22111a3fbb05d513e7e383d83fec5916590d0e7d
Gregory Holder <gregory.holder76@gmail.com>
2020-11-26 17:46:22 +0100

    Ajout de nombreDeJoueursValid dans ControlleurJeu.
    Cette methode indique si les nombres de joueurs bots et humains sont
    valides.

--- Jeu/src/fr/utt/sh/core/Main.java
+++ Jeu/src/fr/utt/sh/core/Main.java
@@ -13,33 +13,25 @@ import fr.utt.sh.console_ui.Utils;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		//Sélection du nombre de joueurs (humains et bots)
-		
-		System.out.println("Combien de joueurs humains et de bots ? (min 0, max 3)");
+		ControlleurJeu c = ControlleurJeu.getInstance();
+
+		// Sélection du nombre de joueurs (humains et bots)
+
+		System.out.println("Combien de joueurs humains et de bots ? (total entre 2 et 3)");
 		String[] input = Utils.getLigneSeparee();
-		
+
 		int nbHumains = Integer.parseInt(input[0]);
-		int nbBots = Integer.parseInt(input[1]);
-		
-		while(nbHumains > 3 || nbHumains < 0 || nbBots > 3 || nbBots < 0) {
-			System.out.println("Arrête de raconter des conneries, utilisateur. Combien de joueurs humains et de bots ? (min 0, max 3)");
+		int nbBots    = Integer.parseInt(input[1]);
+
+		while (!ControlleurJeu.nombreDeJoueursValide(nbHumains, nbBots)) {
+			System.out.println(
+					"Arrête de raconter des conneries, utilisateur. Combien de joueurs humains et de bots ? (min 0, max 3)");
 			input = Utils.getLigneSeparee();
+
 			nbHumains = Integer.parseInt(input[0]);
-			nbBots = Integer.parseInt(input[1]);
-		}
-		
-		if(nbBots + nbHumains > 3) {
-			nbBots = 0;
-			System.out.println("Le nombre total de joueurs ne peut pas excéder 3. Paramètres modifiées à 3 humains et 0 bot.");
+			nbBots    = Integer.parseInt(input[1]);
 		}
 
-		if (nbBots + nbHumains < 2) {
-			nbBots = 2 - (nbBots + nbHumains);
-			System.out.println("Le nombre total de joueurs ne peut pas être inférieur à 2. Paramètres modifiées à "+nbHumains+" humain(s) et "+nbBots+" bot(s).");
-		}
-		
-		ControlleurJeu c = ControlleurJeu.getInstance();
 		c.commencerNouvellePartie(nbHumains, nbBots, Regles.Standard);
 
 		while (!c.jeuTermine()) {
