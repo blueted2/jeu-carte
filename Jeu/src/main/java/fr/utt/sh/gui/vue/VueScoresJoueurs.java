a5cf6436a9601faeacbfcaa51d1adec6c2201f92
Gregory Holder <gregory.holder76@gmail.com>
2021-01-18 10:47:15 +0100

    Simplification boucle for

--- Jeu/src/main/java/fr/utt/sh/gui/vue/VueScoresJoueurs.java
+++ Jeu/src/main/java/fr/utt/sh/gui/vue/VueScoresJoueurs.java
@@ -10,31 +10,41 @@ import javax.swing.JPanel;
 import fr.utt.sh.core.ControlleurJeu;
 import fr.utt.sh.core.Joueur;
 
+/**
+ * Réprésentation graphique des scores des joueurs du jeu.
+ * 
+ * @author grego
+ *
+ */
+@SuppressWarnings("serial")
 public class VueScoresJoueurs extends JPanel implements Observer {
-	
+
 	private ArrayList<JLabel> labelsScores;
-	private ControlleurJeu c;
-	
+	private ControlleurJeu    c;
+
+	/**
+	 * Constructeur pour la vue.
+	 */
 	public VueScoresJoueurs() {
-		c = ControlleurJeu.getInstance();
+		c            = ControlleurJeu.getInstance();
 		labelsScores = new ArrayList<JLabel>();
-		
-		for(Joueur joueur : c.getJoueurs()) {
+
+		for (int i = 0; i < c.getJoueurs().size(); i++) {
 			JLabel label = new JLabel();
 			labelsScores.add(label);
 			add(label);
 		}
 		c.addObserver(this);
 	}
-	
+
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		updateComponentValues();
 	}
-	
+
 	private void updateComponentValues() {
 		ArrayList<Joueur> joueurs = c.getJoueurs();
-		for(int i = 0; i<joueurs.size(); i++) {
+		for (int i = 0; i < joueurs.size(); i++) {
 			Joueur joueur = joueurs.get(i);
 			labelsScores.get(i).setText(String.format("%s : %s", joueur.toString(), joueur.getScore()));
 		}
